8c781025800d739f926a7521777356f11c4d62c3
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2022-04-10 14:09:46 +0200

    Reorganizado el código
    Colocar bien en el css la parte del boton gif

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,9 @@
-const nav = document.querySelector('.menu-navegación-superior')
-window.addEventListener('scroll', fixNav)
+const nav = document.querySelector('.menu-navegación-superior') // creamos la constante sobre el menu-navegacion-superior
+window.addEventListener('scroll', fixNav) //evento al bajan con el srcol
 
 function fixNav() {
     if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
+        nav.classList.add('active') //activa la clase
     } else {
         nav.classList.remove('active')
     }
